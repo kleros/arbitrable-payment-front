90e1c5656ede0c51f7b4737803154be98b0439d1
Nicolas Wagner <contact@wagner-nicolas.com>
2018-02-05 16:06:26 +0100

    style show contract

--- src/containers/contract/index.js
+++ src/containers/contract/index.js
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import Blockies from 'react-blockies'
 
 import { objMap } from '../../utils/functional'
 import * as contractSelectors from '../../reducers/contract'
@@ -38,11 +39,18 @@ class Contract extends PureComponent {
     createPay(match.params.contractAddress)
   }
 
+  shortAddress = address => {
+    const startAddress = address.substr(0, address.length-36)
+    const endAddress = address.substr(37)
+
+    return `${startAddress}...${endAddress}`
+  }
+
   render() {
     const { loadingContract, contract } = this.props
 
     return (
-      <div className="container">
+      <div>
         {renderIf(
           [contract.loading],
           [contract.data && contract.data.partyAFee],
@@ -50,23 +58,49 @@ class Contract extends PureComponent {
           {
             loading: <span>loading</span>,
             done: contract.data && (
-              <div>
-                {objMap(contract, (value, key) => (
-                  <div key={key}>
-                    {key}: {JSON.stringify(value)}
+              <div className="Contract">
+                <div className="Contract-content">
+                  <div className="Contract-content-address">
+                    <div><Blockies seed={contract.data.address} size={6} scale={10} bgColor="#f5f5f5" /></div>
+                    <div className="Contract-content-address-address">{this.shortAddress(contract.data.address)}</div>
                   </div>
-                ))}
-                {!contract.data.partyAFee && !contract.data.partyBFee ?
-                  <div>
-                    <button onClick={this.createDispute}>Create dispute</button>
-                    <button onClick={this.createPay}>Pay</button>
+                  {console.log(contract.data)}
+                  <div className="partyB">
+                    <div className="identicon">
+                      <Blockies seed={contract.data.partyA} size={5} scale={4} bgColor="#f5f5f5" />
+                    </div>
+                    <div className="content">
+                      {this.shortAddress(contract.data.partyA)}
+                    </div>
+
+                    <div>&nbsp;&nbsp;</div>
+
+                    <div className="identicon">
+                      <Blockies seed={contract.data.partyB} size={5} scale={4} bgColor="#f5f5f5" />
+                    </div>
+
+                    <div className="content">
+                      {this.shortAddress(contract.data.partyB)}
+                    </div>
+
                   </div>
-                  :
-                  <div>
-                    Waiting pay from the other party
-                  </div>
-                }
-              </div>
+
+                  <div>Status: {contract.data.status}</div>
+                  <div>Mail: {contract.data.email}</div>
+                  <div className="description">{contract.data.description}</div>
+
+                  {!contract.data.partyAFee && !contract.data.partyBFee ?
+                    <div className="Contract-actions">
+                      <div className="Contract-actions-button Contract-actions-button-left" onClick={this.createDispute}>Create dispute</div>
+                      <div className="Contract-actions-button Contract-actions-button-right" onClick={this.createPay}>Pay</div>
+                    </div>
+                    :
+                    <div>
+                      Waiting pay from the other party
+                    </div>
+                  }
+                </div>
+            </div>
             ),
             failed: contract.failedLoading && 'failedLoading contract'
         })}
