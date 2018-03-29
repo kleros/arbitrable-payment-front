4fe0774d7024c7083458a8098afac1d91e190f58
Thoughtscript <adam.gerard@gmail.com>
2018-03-29 06:21:27 +0100

    refactor:regex

--- src/containers/contract/evidence-form/index.js
+++ src/containers/contract/evidence-form/index.js
@@ -58,7 +58,7 @@ class NewEvidence extends PureComponent {
   }
 
   isUrl = url => {
-    const re = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/g
+    const re = /^(http:\/\/|https:\/\/)(www){0,1}[a-z0-9]+([-.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/g
     return re.test(String(url).toLowerCase())
   }
 
