7df53d477a6ef2453ee24358f10a537a1a796077
Sam Vitello <sam.j.vitello@gmail.com>
2018-07-10 13:47:51 -0600

    feat: swap arbitrator with network (#53)

--- src/bootstrap/dapp-api.js
+++ src/bootstrap/dapp-api.js
@@ -1,24 +1,57 @@
 import Eth from 'ethjs'
 import { Kleros } from 'kleros-api'
 
+import * as ethConstants from '../constants/eth'
+
 const env = process.env.NODE_ENV === 'production' ? 'PROD' : 'DEV'
-export const ARBITRATOR_ADDRESS =
-  process.env[`REACT_APP_${env}_ARBITRATOR_ADDRESS`]
+const ETHEREUM_PROVIDER = process.env[`REACT_APP_${env}_ETHEREUM_PROVIDER`]
+const STORE_PROVIDER = process.env[`REACT_APP_${env}_STORE_PROVIDER`]
 
-let ethInstance
+let eth
 if (process.env.NODE_ENV === 'test')
-  ethInstance = new Eth(require('ethereumjs-testrpc').provider())
+  eth = new Eth(require('ganache-cli').provider())
 else if (window.web3 && window.web3.currentProvider)
-  ethInstance = new Eth(window.web3.currentProvider)
-else
-  ethInstance = new Eth.HttpProvider(
-    process.env[`REACT_APP_${env}_ETHEREUM_PROVIDER`]
-  )
+  eth = new Eth(window.web3.currentProvider)
+else eth = new Eth(new Eth.HttpProvider(ETHEREUM_PROVIDER))
 
-export const eth = ethInstance
+let ARBITRATOR_ADDRESS
+let kleros
+const initializeKleros = async () => {
+  const networkID = await eth.net_version()
 
-export const kleros = new Kleros(
-  ethInstance.currentProvider,
-  process.env.REACT_APP_STORE_PROVIDER,
-  ARBITRATOR_ADDRESS
+  ARBITRATOR_ADDRESS =
+    process.env[
+      `REACT_APP_${env}_${
+        ethConstants.NETWORK_MAP[networkID]
+      }_ARBITRATOR_ADDRESS`
+    ]
+
+  kleros = new Kleros(eth.currentProvider, STORE_PROVIDER, ARBITRATOR_ADDRESS)
+}
+
+const ETHAddressRegExpCaptureGroup = '(0x[a-fA-F0-9]{40})'
+const ETHAddressRegExp = /0x[a-fA-F0-9]{40}/
+const strictETHAddressRegExp = /^0x[a-fA-F0-9]{40}$/
+
+export {
+  eth,
+  ARBITRATOR_ADDRESS,
+  kleros,
+  initializeKleros,
+  ETHAddressRegExpCaptureGroup,
+  ETHAddressRegExp,
+  strictETHAddressRegExp
+}
+
+setTimeout(
+  () =>
+    console.log(
+      'Arbitrator Address: ',
+      ARBITRATOR_ADDRESS,
+      'Kleros: ',
+      kleros,
+      'Web3: ',
+      window.web3
+    ),
+  1000
 )
